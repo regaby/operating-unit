00139ae9e231f278a8047237c2322eebd16b95f5
Gabriela Rivero <regaby@gmail.com>
2020-09-06 20:19:08 -0300

    [FIX] sobrecargo metodo create_invoice para quitar el sudo()

--- pos_operating_unit/models/pos_order.py
+++ pos_operating_unit/models/pos_order.py
@@ -12,14 +12,45 @@ class PosOrder(models.Model):
     operating_unit_id = fields.Many2one(
         comodel_name='operating.unit',
         string='Operating Unit',
-        related='config_id.operating_unit_id',         
+        related='config_id.operating_unit_id',
     )
 
     crm_team_id = fields.Many2one(
-        comodel_name='crm.team', 
-        related='config_id.crm_team_id', 
-        string="Sales Team", 
+        comodel_name='crm.team',
+        related='config_id.crm_team_id',
+        string="Sales Team",
         )
+
+    def _create_invoice(self):
+        self.ensure_one()
+        Invoice = self.env['account.invoice']
+        # Force company for all SUPERUSER_ID action
+        local_context = dict(self.env.context, force_company=self.company_id.id, company_id=self.company_id.id)
+        if self.invoice_id:
+            return self.invoice_id
+
+        if not self.partner_id:
+            raise UserError(_('Please provide a partner for the sale.'))
+
+        invoice = Invoice.new(self._prepare_invoice())
+        invoice._onchange_partner_id()
+        invoice.fiscal_position_id = self.fiscal_position_id
+
+        inv = invoice._convert_to_write({name: invoice[name] for name in invoice._cache})
+        # Tuve que comentar el sudo del create porque chocaba con un metodo check al crear la factura
+        # _check_company_operating_unit: que verficaba el operating unit del usuario y al ser sudo tomaba el admin
+        # new_invoice = Invoice.with_context(local_context).sudo().create(inv)
+        new_invoice = Invoice.with_context(local_context).create(inv)
+        message = _("This invoice has been created from the point of sale session: <a href=# data-oe-model=pos.order data-oe-id=%d>%s</a>") % (self.id, self.name)
+        new_invoice.message_post(body=message)
+        self.write({'invoice_id': new_invoice.id, 'state': 'invoiced'})
+
+        for line in self.lines:
+            self.with_context(local_context)._action_create_invoice_line(line, new_invoice.id)
+
+        new_invoice.with_context(local_context).compute_taxes()
+        self.sudo().write({'state': 'invoiced'})
+        return new_invoice
 
 
 class PosOrderLine(models.Model):
